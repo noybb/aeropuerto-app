7409fb4a20b4ed6f4b92fc084959495d1ada250c
noybb <lensko_2002@hotmail.com>
2025-10-31 03:35:21 -0300

    Header, footer y home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
- import { Component } from '@angular/core';
-  import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
 
   @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',  // ✅ Correcto
-    styleUrls: ['./app.component.css']    // ✅ Correcto
+    selector: 'app-root',  // El nombre del componente en HTML
+    standalone: true,   // Componente independiente (nueva forma de Angular)
+    imports: [RouterOutlet, Header],  // Importamos RouterOutlet para usar las rutas
+    templateUrl: './app.component.html',  // Ruta al archivo HTML
+    styleUrls: ['./app.component.css']    // Ruta al archivo CSS
   })
   export class AppComponent {
     title = 'Aeropuerto App';
